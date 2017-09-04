137fad1e776c364cc514fb6232c60d7d89384d23
Usman Ahmad <usman.ahmad@gosign.de>
2017-09-04 13:04:31 +0500

    first commit yes it is first

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "news".
+ *
+ * Auto generated | Identifier: b407c2ef8ec942ceb36f332c91e4ff3a
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Gosign Teaser',
+	'description' => 'Teaser',
+	'category' => 'fe',
+	'author' => 'Usman Ahmad',
+	'author_email' => 'usman.ahmad@gosign.de',
+	'state' => 'stable',
+	'clearCacheOnLoad' => 1,
+	'version' => '1.0.0',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '7.6.13-8.7.99',
+		),
+		'conflicts' =>
+		array (
+		),
+	),
+	'createDirs' => NULL,
+	'clearcacheonload' => true,
+	'author_company' => NULL,
+	'_md5_values_when_last_written' => '',
+);
+
+?>
